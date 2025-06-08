169fdbce3bc0965c5ffc9fa2ca17dc64ead56508
Yordy Carmona <yordy.carmona8@gmail.com>
2025-06-07 21:18:07 -0400

    feat: add class helpers for some validations

---
+++ src/Helpers.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace clinica_dental_ev03
+{
+    class Helpers
+    {
+        public void SoloNumeros(KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        public bool ValidarEmail(string email)
+        {
+            // Regex con el formato de un correo.
+            string formato = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+            // Comparar el 'email' con el fomato, por medio del regex.
+            if (Regex.Replace(email, formato, string.Empty).Length == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string FormatearRun(string rut)
+        {
+            int cont = 0;
+            string format;
+            if (rut.Length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                rut = rut.Replace(".", "");
+                rut = rut.Replace("-", "");
+                format = "-" + rut.Substring(rut.Length - 1);
+                for (int i = rut.Length - 2; i >= 0; i--)
+                {
+                    format = rut.Substring(i, 1) + format;
+                    cont++;
+                    if (cont == 3 && i != 0)
+                    {
+                        format = "." + format;
+                        cont = 0;
+                    }
+                }
+            }
+            return format;
+        }
+
+        public bool ValidarRun(string rut)
+        {
+            bool validacion = false;
+            try
+            {
+                rut = rut.ToUpper();
+                rut = rut.Replace(".", "");
+                rut = rut.Replace("-", "");
+                int rutAux = int.Parse(rut.Substring(0, rut.Length - 1));
+
+                char dv = char.Parse(rut.Substring(rut.Length - 1, 1));
+
+                int m = 0, s = 1;
+                for (; rutAux != 0; rutAux /= 10)
+                {
+                    s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+                }
+
+                if (dv == (char)(s != 0 ? s + 47 : 75))
+                {
+                    validacion = true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return validacion;
+        }
+    }
+}
+
