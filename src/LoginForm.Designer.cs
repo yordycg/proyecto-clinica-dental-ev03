7ab1123d5a1256b533517b48834c9d376a27dfb8
Yordy Carmona <yordy.carmona8@gmail.com>
2025-06-09 21:47:40 -0400

    feat: add login form

---
+++ src/LoginForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace clinica_dental_ev03
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            txtUsuario = new TextBox();
+            textBox1 = new TextBox();
+            btnIngresar = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(67, 181);
+            label1.Name = "label1";
+            label1.Size = new Size(182, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Ingresar Datos";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.logo_clinica_dental;
+            pictureBox1.Location = new Point(69, 25);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(180, 153);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(30, 225);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(30, 294);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 21);
+            label3.TabIndex = 3;
+            label3.Text = "Contraseña:";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtUsuario.Location = new Point(30, 249);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(253, 33);
+            txtUsuario.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(30, 318);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(253, 33);
+            textBox1.TabIndex = 5;
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.BackColor = Color.Green;
+            btnIngresar.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnIngresar.ForeColor = Color.White;
+            btnIngresar.Location = new Point(92, 359);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(134, 40);
+            btnIngresar.TabIndex = 6;
+            btnIngresar.Text = "Ingresar";
+            btnIngresar.UseVisualStyleBackColor = false;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(334, 411);
+            Controls.Add(btnIngresar);
+            Controls.Add(textBox1);
+            Controls.Add(txtUsuario);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtUsuario;
+        private TextBox textBox1;
+        private Button btnIngresar;
+    }
+}
