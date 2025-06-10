5a769b0ec9ff2a61fedd5d078249efbe0b256d99
Yordy Carmona <yordy.carmona8@gmail.com>
2025-06-10 12:01:55 -0400

    feat: update sql schema with new tables and changes in some columns

--- src/SQL-script/database.sql
+++ src/SQL-script/database.sql
@@ -11,20 +11,37 @@ CREATE TABLE pacientes (
     fecha_registro DATE DEFAULT GETDATE()
 );
 
-CREATE TABLE dentistas (
-    dentista_id INT PRIMARY KEY IDENTITY(1,1),
+CREATE TABLE tipos_empleados (
+    tipo_id INT PRIMARY KEY IDENTITY(1,1),
+    nombre VARCHAR(50) NOT NULL UNIQUE -- dentista | secretari@ | auxiliar | administrador
+);
+
+CREATE TABLE empleados (
+    run VARCHAR(15) PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
+    tipo_empleado_id INT NOT NULL,
+    sexo CHAR NOT NULL CHECK (sexo in ('M', 'F')),
     telefono VARCHAR(15),
     correo VARCHAR(100),
-    fecha_registro DATE DEFAULT GETDATE()
+    fecha_registro DATE DEFAULT GETDATE(),
+    FOREIGN KEY (tipo_empleado_id) REFERENCES tipos_empleados (tipo_id)
+);
+
+CREATE TABLE usuarios (
+    usuario_id INT PRIMARY KEY IDENTITY(1,1),
+    empleado_run VARCHAR(15) NOT NULL,
+    nombre VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    fecha_registro DATE DEFAULT GETDATE(),
+    FOREIGN KEY (empleado_run) REFERENCES empleados (run)
 );
 
 CREATE TABLE servicios (
     servicio_id INT PRIMARY KEY IDENTITY(1,1),
     nombre VARCHAR(100) NOT NULL UNIQUE,
     costo INT NOT NULL CHECK(costo > 0) DEFAULT 0,
-    duracion INT NOT NULL
+    duracion INT NOT NULL --  minutos
 );
 
 -- CREATE TABLE estado_cita (
@@ -35,12 +52,14 @@ CREATE TABLE servicios (
 CREATE TABLE citas (
     cita_id INT PRIMARY KEY IDENTITY(1,1),
     run_paciente VARCHAR(15) NOT NULL,
-    dentista_id INT NOT NULL,
+    dentista_id VARCHAR(15) NOT NULL, -- solo dentista
     estado VARCHAR(50) NOT NULL,
     fecha DATE NOT NULL,
     hora TIME NOT NULL,
+    creado_por VARCHAR(15) NOT NULL,  -- secretari@ | dentista | etc.
     FOREIGN KEY (run_paciente) REFERENCES pacientes (run),
-    FOREIGN KEY (dentista_id) REFERENCES dentistas (dentista_id),
+    FOREIGN KEY (dentista_id) REFERENCES empleados (run),
+    FOREIGN KEY (creado_por) REFERENCES empleados (run)
 );
 
 CREATE TABLE detalle_cita ( -- detalle cita?
@@ -61,4 +80,9 @@ CREATE TABLE pagos (
     FOREIGN KEY (cita_id) REFERENCES citas (cita_id)
 );
 
-SELECT * FROM pacientes;
+-- Insertar datos iniciales...
+INSERT INTO tipos_empleados VALUES
+('Dentista'),
+('Auxiliar'),
+('Secretari@'),
+('Admin');
