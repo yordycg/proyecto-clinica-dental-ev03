7e6aa46347f015a3f84b27d48ada75a6dbeab56e
Yordy Carmona <yordy.carmona8@gmail.com>
2025-06-11 19:37:47 -0400

    feat: update LoginForm layout and behavior

--- src/LoginForm.Designer.cs
+++ src/LoginForm.Designer.cs
@@ -33,8 +33,9 @@
             label2 = new Label();
             label3 = new Label();
             txtUsuario = new TextBox();
-            textBox1 = new TextBox();
+            txtPass = new TextBox();
             btnIngresar = new Button();
+            btnRegistrar = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -42,18 +43,19 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(67, 181);
+            label1.Location = new Point(77, 241);
             label1.Name = "label1";
-            label1.Size = new Size(182, 32);
+            label1.Size = new Size(223, 41);
             label1.TabIndex = 0;
             label1.Text = "Ingresar Datos";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.logo_clinica_dental;
-            pictureBox1.Location = new Point(69, 25);
+            pictureBox1.Location = new Point(79, 33);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(180, 153);
+            pictureBox1.Size = new Size(206, 204);
             pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
@@ -62,9 +64,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(30, 225);
+            label2.Location = new Point(52, 289);
             label2.Name = "label2";
-            label2.Size = new Size(69, 21);
+            label2.Size = new Size(86, 28);
             label2.TabIndex = 2;
             label2.Text = "Usuario:";
             // 
@@ -72,52 +74,74 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(30, 294);
+            label3.Location = new Point(52, 381);
             label3.Name = "label3";
-            label3.Size = new Size(96, 21);
+            label3.Size = new Size(119, 28);
             label3.TabIndex = 3;
             label3.Text = "Contraseña:";
             // 
             // txtUsuario
             // 
             txtUsuario.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtUsuario.Location = new Point(30, 249);
+            txtUsuario.Location = new Point(52, 321);
+            txtUsuario.Margin = new Padding(3, 4, 3, 4);
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(253, 33);
+            txtUsuario.Size = new Size(280, 39);
             txtUsuario.TabIndex = 4;
             // 
-            // textBox1
+            // txtPass
             // 
-            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(30, 318);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(253, 33);
-            textBox1.TabIndex = 5;
+            txtPass.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPass.Location = new Point(52, 413);
+            txtPass.Margin = new Padding(3, 4, 3, 4);
+            txtPass.Name = "txtPass";
+            txtPass.PasswordChar = '*';
+            txtPass.Size = new Size(280, 39);
+            txtPass.TabIndex = 5;
+            txtPass.KeyPress += txtPass_KeyPress;
             // 
             // btnIngresar
             // 
             btnIngresar.BackColor = Color.Green;
-            btnIngresar.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnIngresar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnIngresar.ForeColor = Color.White;
-            btnIngresar.Location = new Point(92, 359);
+            btnIngresar.Location = new Point(212, 474);
+            btnIngresar.Margin = new Padding(3, 4, 3, 4);
             btnIngresar.Name = "btnIngresar";
-            btnIngresar.Size = new Size(134, 40);
+            btnIngresar.Size = new Size(120, 45);
             btnIngresar.TabIndex = 6;
             btnIngresar.Text = "Ingresar";
             btnIngresar.UseVisualStyleBackColor = false;
+            btnIngresar.Click += btnIngresar_Click;
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.BackColor = Color.LightSlateGray;
+            btnRegistrar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnRegistrar.ForeColor = Color.Black;
+            btnRegistrar.Location = new Point(52, 474);
+            btnRegistrar.Margin = new Padding(3, 4, 3, 4);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(120, 45);
+            btnRegistrar.TabIndex = 7;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = false;
+            btnRegistrar.Click += btnRegistrar_Click;
             // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(334, 411);
+            ClientSize = new Size(382, 548);
+            Controls.Add(btnRegistrar);
             Controls.Add(btnIngresar);
-            Controls.Add(textBox1);
+            Controls.Add(txtPass);
             Controls.Add(txtUsuario);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(pictureBox1);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "LoginForm";
             Text = "LoginForm";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -132,7 +156,8 @@
         private Label label2;
         private Label label3;
         private TextBox txtUsuario;
-        private TextBox textBox1;
+        private TextBox txtPass;
         private Button btnIngresar;
+        private Button btnRegistrar;
     }
 }
--- src/LoginForm.cs
+++ src/LoginForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using clinica_dental_ev03.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,80 @@ namespace clinica_dental_ev03
 {
     public partial class LoginForm : Form
     {
+        ClinicaDentalDbContext db = new();
+        Helpers h = new();
+        public static int? usuarioId = null;
+        public static string? empleadoRun = null;
+
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void CleanForm()
+        {
+            txtUsuario.Text = "";
+            txtPass.Text = "";
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            Logg();
+        }
+
+        private void Logg()
+        {
+            string? err = null;
+            if (txtUsuario.Text.Trim() == "")
+            {
+                err = "Debe ingresar el 'USUARIO'.\n";
+            }
+            if (txtPass.Text.Trim() == "")
+            {
+                err += "Debe ingresar 'CONTRASEÑA'.\n";
+            }
+            if (err != null)
+            {
+                MessageBox.Show(err, "Error de Validacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var user = db.Usuarios.FirstOrDefault(u => u.Nombre == txtUsuario.Text && u.Password == txtPass.Text); ;
+
+                if (user != null)
+                {
+                    usuarioId = user.UsuarioId;
+                    empleadoRun = user.EmpleadoRun;
+                    MessageBox.Show($"Bienvenido {user.Nombre}", "Ingreso Exitoso!");
+                    MenuForm newMenuForm = new();
+                    newMenuForm.WindowState = FormWindowState.Maximized; // abrir en vista maximizada.
+                    newMenuForm.Show();
+                    CleanForm();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Email y/o Contraseña incorrecta", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void txtPass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) // techa 'Enter'.
+            {
+                Logg();
+            }
+        }
+
+        // NOTE: esta funcion o caracteristica es necesaria? 
+        // No sera mejor entrar al menuform, y de ahi registrar nuevos usuarios?
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            CleanForm();
+            EmpleadosForm newEmpleadoForm = new();
+            newEmpleadoForm.Show();
+            this.Hide();
         }
     }
 }
