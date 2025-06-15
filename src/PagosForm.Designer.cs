2f9db0a4279aafb8b5ea12c8e32acf795d96f945
Yordy Carmona <yordy.carmona8@gmail.com>
2025-06-15 18:45:29 -0400

    feat: add form pagos and implement CRUD funtionality

---
+++ src/PagosForm.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace clinica_dental_ev03
+{
+    partial class PagosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvCitas = new DataGridView();
+            btnEliminar = new Button();
+            btnGuardar = new Button();
+            btnLimpiar = new Button();
+            dgvPagos = new DataGridView();
+            cbMetodoPago = new ComboBox();
+            label5 = new Label();
+            cbEstadoPago = new ComboBox();
+            label8 = new Label();
+            txtCitaId = new TextBox();
+            label2 = new Label();
+            label4 = new Label();
+            txtMonto = new TextBox();
+            label3 = new Label();
+            dtpFecha = new DateTimePicker();
+            pnDGVCitas = new Panel();
+            pnForm = new Panel();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dgvCitas).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvPagos).BeginInit();
+            pnForm.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dgvCitas
+            // 
+            dgvCitas.BackgroundColor = Color.White;
+            dgvCitas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCitas.Location = new Point(50, 25);
+            dgvCitas.Name = "dgvCitas";
+            dgvCitas.Size = new Size(900, 150);
+            dgvCitas.TabIndex = 0;
+            dgvCitas.MouseClick += dgvCitas_MouseClick;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.BackColor = Color.IndianRed;
+            btnEliminar.FlatAppearance.BorderSize = 0;
+            btnEliminar.FlatStyle = FlatStyle.Flat;
+            btnEliminar.Font = new Font("Nirmala UI", 9F);
+            btnEliminar.ForeColor = Color.White;
+            btnEliminar.Location = new Point(137, 295);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(75, 23);
+            btnEliminar.TabIndex = 18;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = false;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.BackColor = Color.FromArgb(52, 143, 108);
+            btnGuardar.FlatAppearance.BorderSize = 0;
+            btnGuardar.FlatStyle = FlatStyle.Flat;
+            btnGuardar.Font = new Font("Nirmala UI", 9F);
+            btnGuardar.ForeColor = Color.White;
+            btnGuardar.Location = new Point(228, 295);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(75, 23);
+            btnGuardar.TabIndex = 17;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = false;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.BackColor = Color.Gainsboro;
+            btnLimpiar.FlatAppearance.BorderSize = 0;
+            btnLimpiar.FlatStyle = FlatStyle.Flat;
+            btnLimpiar.Font = new Font("Nirmala UI", 9F);
+            btnLimpiar.Location = new Point(46, 295);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(75, 23);
+            btnLimpiar.TabIndex = 16;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = false;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // dgvPagos
+            // 
+            dgvPagos.BackgroundColor = Color.White;
+            dgvPagos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvPagos.Location = new Point(350, 200);
+            dgvPagos.Name = "dgvPagos";
+            dgvPagos.Size = new Size(600, 300);
+            dgvPagos.TabIndex = 19;
+            dgvPagos.MouseClick += dgvPagos_MouseClick;
+            // 
+            // cbMetodoPago
+            // 
+            cbMetodoPago.FormattingEnabled = true;
+            cbMetodoPago.Location = new Point(153, 201);
+            cbMetodoPago.Name = "cbMetodoPago";
+            cbMetodoPago.Size = new Size(150, 23);
+            cbMetodoPago.TabIndex = 51;
+            // 
+            // label5
+            // 
+            label5.Location = new Point(29, 201);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 23);
+            label5.TabIndex = 50;
+            label5.Text = "Metodo Pago";
+            label5.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // cbEstadoPago
+            // 
+            cbEstadoPago.FormattingEnabled = true;
+            cbEstadoPago.Items.AddRange(new object[] { "Femenino", "Masculino" });
+            cbEstadoPago.Location = new Point(153, 240);
+            cbEstadoPago.Name = "cbEstadoPago";
+            cbEstadoPago.Size = new Size(150, 23);
+            cbEstadoPago.TabIndex = 49;
+            // 
+            // label8
+            // 
+            label8.Location = new Point(29, 240);
+            label8.Name = "label8";
+            label8.Size = new Size(100, 23);
+            label8.TabIndex = 48;
+            label8.Text = "Estado Pago";
+            label8.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // txtCitaId
+            // 
+            txtCitaId.Location = new Point(153, 84);
+            txtCitaId.Name = "txtCitaId";
+            txtCitaId.Size = new Size(150, 23);
+            txtCitaId.TabIndex = 47;
+            // 
+            // label2
+            // 
+            label2.Location = new Point(29, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 23);
+            label2.TabIndex = 46;
+            label2.Text = "Num Cita";
+            label2.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            label4.Location = new Point(29, 162);
+            label4.Name = "label4";
+            label4.Size = new Size(100, 23);
+            label4.TabIndex = 44;
+            label4.Text = "Fecha";
+            label4.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // txtMonto
+            // 
+            txtMonto.Location = new Point(153, 123);
+            txtMonto.Name = "txtMonto";
+            txtMonto.Size = new Size(150, 23);
+            txtMonto.TabIndex = 43;
+            txtMonto.KeyPress += txtMonto_KeyPress;
+            // 
+            // label3
+            // 
+            label3.Location = new Point(29, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 23);
+            label3.TabIndex = 42;
+            label3.Text = "Monto";
+            label3.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // dtpFecha
+            // 
+            dtpFecha.CustomFormat = "dd/MM/yyyy";
+            dtpFecha.Format = DateTimePickerFormat.Custom;
+            dtpFecha.Location = new Point(153, 163);
+            dtpFecha.Name = "dtpFecha";
+            dtpFecha.Size = new Size(150, 23);
+            dtpFecha.TabIndex = 52;
+            // 
+            // pnDGVCitas
+            // 
+            pnDGVCitas.Dock = DockStyle.Top;
+            pnDGVCitas.Location = new Point(0, 0);
+            pnDGVCitas.Name = "pnDGVCitas";
+            pnDGVCitas.Size = new Size(1000, 150);
+            pnDGVCitas.TabIndex = 53;
+            // 
+            // pnForm
+            // 
+            pnForm.Controls.Add(dtpFecha);
+            pnForm.Controls.Add(cbMetodoPago);
+            pnForm.Controls.Add(label5);
+            pnForm.Controls.Add(cbEstadoPago);
+            pnForm.Controls.Add(label8);
+            pnForm.Controls.Add(txtCitaId);
+            pnForm.Controls.Add(label2);
+            pnForm.Controls.Add(label4);
+            pnForm.Controls.Add(txtMonto);
+            pnForm.Controls.Add(label3);
+            pnForm.Controls.Add(btnEliminar);
+            pnForm.Controls.Add(btnGuardar);
+            pnForm.Controls.Add(btnLimpiar);
+            pnForm.Dock = DockStyle.Left;
+            pnForm.Location = new Point(0, 150);
+            pnForm.Name = "pnForm";
+            pnForm.Size = new Size(350, 400);
+            pnForm.TabIndex = 54;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(350, 150);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(650, 400);
+            panel1.TabIndex = 55;
+            // 
+            // PagosForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1000, 550);
+            Controls.Add(dgvCitas);
+            Controls.Add(pnForm);
+            Controls.Add(pnDGVCitas);
+            Controls.Add(dgvPagos);
+            Controls.Add(panel1);
+            Font = new Font("Nirmala UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "PagosForm";
+            Text = "PagosForm";
+            ((System.ComponentModel.ISupportInitialize)dgvCitas).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvPagos).EndInit();
+            pnForm.ResumeLayout(false);
+            pnForm.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvCitas;
+        private Button btnEliminar;
+        private Button btnGuardar;
+        private Button btnLimpiar;
+        private DataGridView dgvPagos;
+        private ComboBox cbMetodoPago;
+        private Label label5;
+        private ComboBox cbEstadoPago;
+        private Label label8;
+        private TextBox txtCitaId;
+        private Label label2;
+        private Label label4;
+        private TextBox txtMonto;
+        private Label label3;
+        private DateTimePicker dtpFecha;
+        private Panel pnDGVCitas;
+        private Panel pnForm;
+        private Panel panel1;
+    }
+}
---
+++ src/PagosForm.cs
@@ -0,0 +1,177 @@
+﻿using clinica_dental_ev03.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clinica_dental_ev03
+{
+    public partial class PagosForm : Form
+    {
+        ClinicaDentalDbContext db = new();
+        Helpers h = new();
+        int? idPago = null;
+
+        public PagosForm()
+        {
+            InitializeComponent();
+            LoadMetodoPago();
+            LoadEstadoPago();
+            ShowCitas();
+            ShowPagos();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            CleanForm();
+        }
+
+        private void CleanForm()
+        {
+            txtCitaId.Text = "";
+            txtMonto.Text = "";
+            dtpFecha.Value = DateTime.Now;
+            cbMetodoPago.SelectedIndex = -1;
+            cbEstadoPago.SelectedIndex = -1;
+            idPago = null;
+        }
+
+        private void LoadMetodoPago()
+        {
+            cbMetodoPago.Items.Clear();
+            cbMetodoPago.Items.Add("Efectivo");
+            cbMetodoPago.Items.Add("Tarjeta de Credito");
+            cbMetodoPago.Items.Add("Transferencia");
+            cbMetodoPago.SelectedIndex = -1; // default.
+        }
+
+        private void LoadEstadoPago()
+        {
+            cbEstadoPago.Items.Clear();
+            cbEstadoPago.Items.Add("Pagado");
+            cbEstadoPago.Items.Add("Pendiente");
+            cbEstadoPago.Items.Add("Reembolso");
+            cbEstadoPago.SelectedIndex = -1; // default.
+        }
+
+        private void ShowCitas()
+        {
+            var citasCompletadas = db.Citas.Where(c => c.Estado == "Completada").ToList();
+            dgvCitas.DataSource = citasCompletadas;
+            dgvCitas.Columns[6].Visible = false;
+            dgvCitas.Columns[7].Visible = false;
+            dgvCitas.Columns[8].Visible = false;
+            dgvCitas.Columns[9].Visible = false;
+            dgvCitas.Columns[10].Visible = false;
+            dgvCitas.Columns[11].Visible = false;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string? err = null;
+
+            if (txtCitaId.Text.Trim() == "")
+                err = "Debe seleccionar una 'CITA'.\n";
+            if (txtMonto.Text.Trim() == "")
+                err += "Debe ingresar un 'MONTO'.\n";
+            if (cbMetodoPago.SelectedIndex == -1)
+                err += "Debe seleccionar un 'METODO DE PAGO'.\n";
+            if (cbEstadoPago.SelectedIndex == -1)
+                err += "Debe seleccionar un 'ESTADO DEL PAGO'.\n";
+
+            if (err != null)
+            {
+                MessageBox.Show(err, "Error de Validacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                DateOnly fechaPago = DateOnly.FromDateTime(dtpFecha.Value);
+
+                if (idPago == null)
+                {
+                    Pago newPago = new();
+                    newPago.CitaId = int.Parse(txtCitaId.Text);
+                    newPago.Monto = int.Parse(txtMonto.Text);
+                    newPago.Fecha = dtpFecha.Value;
+                    newPago.MetodoPago = cbMetodoPago.Text;
+                    newPago.EstadoPago = cbEstadoPago.Text;
+
+                    db.Pagos.Add(newPago);
+
+                    MessageBox.Show("Pago registrado con exito!");
+                }
+                else
+                {
+                    Pago? foundPago = db.Pagos.Find(idPago);
+                    if (foundPago != null)
+                    {
+                        foundPago.CitaId = int.Parse(txtCitaId.Text);
+                        foundPago.Monto = int.Parse(txtMonto.Text);
+                        foundPago.Fecha = dtpFecha.Value;
+                        foundPago.MetodoPago = cbMetodoPago.Text;
+                        foundPago.EstadoPago = cbEstadoPago.Text;
+                    }
+
+                    MessageBox.Show("Pago modificado con exito!");
+                }
+                db.SaveChanges();
+                CleanForm();
+                ShowPagos();
+            }
+        }
+
+        private void ShowPagos()
+        {
+            var pagos = db.Pagos.ToList();
+            dgvPagos.DataSource = pagos;
+            dgvPagos.Columns[0].Visible = false;
+            dgvPagos.Columns[6].Visible = false;
+        }
+
+        private void txtMonto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            h.SoloNumeros(e);
+        }
+
+        private void dgvCitas_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtCitaId.Text = dgvCitas.CurrentRow.Cells[0].Value.ToString();
+        }
+
+        private void dgvPagos_MouseClick(object sender, MouseEventArgs e)
+        {
+            idPago = int.Parse(dgvPagos.CurrentRow.Cells[0].Value.ToString());
+            txtCitaId.Text = dgvPagos.CurrentRow.Cells[1].Value.ToString();
+            txtMonto.Text = dgvPagos.CurrentRow.Cells[2].Value.ToString();
+            dtpFecha.Value = DateTime.Parse(dgvPagos.CurrentRow.Cells[3].Value.ToString());
+            cbMetodoPago.Text = dgvPagos.CurrentRow.Cells[4].Value.ToString();
+            cbEstadoPago.Text = dgvPagos.CurrentRow.Cells[5].Value.ToString();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (idPago != null)
+            {
+                var resp = MessageBox.Show($"Desea cancelar el pago del paciente de RUT: CAMBIAR?", "Cancelar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (resp == DialogResult.Yes)
+                {
+                    Pago? foundPago = db.Pagos.Find(idPago);
+                    if (foundPago != null)
+                    {
+                        foundPago.EstadoPago = "Cancelada";
+
+                        db.SaveChanges();
+                        CleanForm();
+                        ShowPagos();
+                    }
+                }
+            }
+        }
+    }
+}
