a10257205b44f2591429c5de73f5295937ee3203
Yordy Carmona <yordy.carmona8@gmail.com>
2025-06-21 14:34:50 -0400

    feat: update CitasForm

--- src/CitaForm.Designer.cs
+++ src/CitaForm.Designer.cs
@@ -52,11 +52,13 @@
             // cbEstados
             // 
             cbEstados.BackColor = Color.White;
+            cbEstados.DropDownStyle = ComboBoxStyle.DropDownList;
             cbEstados.FormattingEnabled = true;
             cbEstados.Items.AddRange(new object[] { "Programada", "Completada", "Cancelada", "Reprogramada" });
-            cbEstados.Location = new Point(133, 229);
+            cbEstados.Location = new Point(134, 212);
+            cbEstados.Margin = new Padding(3, 4, 3, 4);
             cbEstados.Name = "cbEstados";
-            cbEstados.Size = new Size(150, 23);
+            cbEstados.Size = new Size(171, 28);
             cbEstados.TabIndex = 34;
             // 
             // dgvCitas
@@ -77,9 +79,10 @@
             btnCancelar.FlatStyle = FlatStyle.Flat;
             btnCancelar.Font = new Font("Nirmala UI", 9F);
             btnCancelar.ForeColor = Color.White;
-            btnCancelar.Location = new Point(133, 363);
+            btnCancelar.Location = new Point(134, 391);
+            btnCancelar.Margin = new Padding(3, 4, 3, 4);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.Size = new Size(86, 31);
             btnCancelar.TabIndex = 32;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = false;
@@ -92,9 +95,10 @@
             btnGuardar.FlatStyle = FlatStyle.Flat;
             btnGuardar.Font = new Font("Nirmala UI", 9F);
             btnGuardar.ForeColor = Color.White;
-            btnGuardar.Location = new Point(224, 363);
+            btnGuardar.Location = new Point(238, 391);
+            btnGuardar.Margin = new Padding(3, 4, 3, 4);
             btnGuardar.Name = "btnGuardar";
-            btnGuardar.Size = new Size(75, 23);
+            btnGuardar.Size = new Size(86, 31);
             btnGuardar.TabIndex = 31;
             btnGuardar.Text = "Guardar";
             btnGuardar.UseVisualStyleBackColor = false;
@@ -107,9 +111,10 @@
             btnLimpiar.FlatStyle = FlatStyle.Flat;
             btnLimpiar.Font = new Font("Nirmala UI", 9F);
             btnLimpiar.ForeColor = SystemColors.ControlText;
-            btnLimpiar.Location = new Point(42, 363);
+            btnLimpiar.Location = new Point(30, 391);
+            btnLimpiar.Margin = new Padding(3, 4, 3, 4);
             btnLimpiar.Name = "btnLimpiar";
-            btnLimpiar.Size = new Size(75, 23);
+            btnLimpiar.Size = new Size(86, 31);
             btnLimpiar.TabIndex = 30;
             btnLimpiar.Text = "Limpiar";
             btnLimpiar.UseVisualStyleBackColor = false;
@@ -118,9 +123,9 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(58, 313);
+            label7.Location = new Point(48, 324);
             label7.Name = "label7";
-            label7.Size = new Size(33, 15);
+            label7.Size = new Size(42, 20);
             label7.TabIndex = 28;
             label7.Text = "Hora";
             label7.TextAlign = ContentAlignment.MiddleLeft;
@@ -128,9 +133,9 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(58, 274);
+            label6.Location = new Point(48, 272);
             label6.Name = "label6";
-            label6.Size = new Size(38, 15);
+            label6.Size = new Size(47, 20);
             label6.TabIndex = 26;
             label6.Text = "Fecha";
             label6.TextAlign = ContentAlignment.MiddleLeft;
@@ -138,9 +143,9 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(58, 232);
+            label5.Location = new Point(48, 216);
             label5.Name = "label5";
-            label5.Size = new Size(42, 15);
+            label5.Size = new Size(54, 20);
             label5.TabIndex = 25;
             label5.Text = "Estado";
             label5.TextAlign = ContentAlignment.MiddleLeft;
@@ -150,10 +155,10 @@
             txtRun.BackColor = Color.White;
             txtRun.BorderStyle = BorderStyle.FixedSingle;
             txtRun.ForeColor = Color.FromArgb(6, 20, 27);
-            txtRun.Location = new Point(133, 152);
-            txtRun.Multiline = true;
+            txtRun.Location = new Point(134, 110);
+            txtRun.Margin = new Padding(3, 4, 3, 4);
             txtRun.Name = "txtRun";
-            txtRun.Size = new Size(150, 23);
+            txtRun.Size = new Size(171, 27);
             txtRun.TabIndex = 20;
             txtRun.TextChanged += txtRun_TextChanged;
             txtRun.Leave += txtRun_Leave;
@@ -162,9 +167,9 @@
             // 
             label2.AutoSize = true;
             label2.ForeColor = Color.FromArgb(6, 20, 27);
-            label2.Location = new Point(58, 154);
+            label2.Location = new Point(48, 112);
             label2.Name = "label2";
-            label2.Size = new Size(28, 15);
+            label2.Size = new Size(34, 20);
             label2.TabIndex = 19;
             label2.Text = "Run";
             label2.TextAlign = ContentAlignment.MiddleLeft;
@@ -173,18 +178,20 @@
             // 
             dtpFechaCita.CustomFormat = "dd/MM/yyyy";
             dtpFechaCita.Format = DateTimePickerFormat.Custom;
-            dtpFechaCita.Location = new Point(133, 268);
+            dtpFechaCita.Location = new Point(134, 264);
+            dtpFechaCita.Margin = new Padding(3, 4, 3, 4);
             dtpFechaCita.Name = "dtpFechaCita";
-            dtpFechaCita.Size = new Size(150, 23);
+            dtpFechaCita.Size = new Size(171, 27);
             dtpFechaCita.TabIndex = 35;
             // 
             // dtpHoraCita
             // 
             dtpHoraCita.Format = DateTimePickerFormat.Time;
-            dtpHoraCita.Location = new Point(133, 307);
+            dtpHoraCita.Location = new Point(134, 316);
+            dtpHoraCita.Margin = new Padding(3, 4, 3, 4);
             dtpHoraCita.Name = "dtpHoraCita";
             dtpHoraCita.ShowUpDown = true;
-            dtpHoraCita.Size = new Size(150, 23);
+            dtpHoraCita.Size = new Size(171, 27);
             dtpHoraCita.TabIndex = 36;
             dtpHoraCita.Value = new DateTime(2025, 6, 8, 15, 0, 0, 0);
             // 
@@ -205,6 +212,7 @@
             pnForm.Controls.Add(label2);
             pnForm.Dock = DockStyle.Left;
             pnForm.Location = new Point(0, 0);
+            pnForm.Margin = new Padding(3, 4, 3, 4);
             pnForm.Name = "pnForm";
             pnForm.Size = new Size(350, 550);
             pnForm.TabIndex = 39;
@@ -212,19 +220,21 @@
             // cbDentistas
             // 
             cbDentistas.BackColor = SystemColors.Window;
+            cbDentistas.DropDownStyle = ComboBoxStyle.DropDownList;
             cbDentistas.FormattingEnabled = true;
             cbDentistas.Items.AddRange(new object[] { "Programada", "Completada", "Cancelada", "Reprogramada" });
-            cbDentistas.Location = new Point(133, 191);
+            cbDentistas.Location = new Point(134, 162);
+            cbDentistas.Margin = new Padding(3, 4, 3, 4);
             cbDentistas.Name = "cbDentistas";
-            cbDentistas.Size = new Size(150, 23);
+            cbDentistas.Size = new Size(171, 28);
             cbDentistas.TabIndex = 40;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(58, 194);
+            label1.Location = new Point(48, 166);
             label1.Name = "label1";
-            label1.Size = new Size(50, 15);
+            label1.Size = new Size(64, 20);
             label1.TabIndex = 39;
             label1.Text = "Dentista";
             label1.TextAlign = ContentAlignment.MiddleLeft;
@@ -234,19 +244,21 @@
             pnDGV.Controls.Add(dgvCitas);
             pnDGV.Dock = DockStyle.Right;
             pnDGV.Location = new Point(350, 0);
+            pnDGV.Margin = new Padding(3, 4, 3, 4);
             pnDGV.Name = "pnDGV";
             pnDGV.Size = new Size(650, 550);
             pnDGV.TabIndex = 40;
             // 
             // CitaForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1000, 550);
             Controls.Add(pnDGV);
             Controls.Add(pnForm);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "CitaForm";
             Text = "CitaForm";
             ((System.ComponentModel.ISupportInitialize)dgvCitas).EndInit();
